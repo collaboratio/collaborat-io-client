f5200b29b23b244f6d6b0d165fe8f078f76441c9
Fernando Jiménez <fernando.jimenez.rivera@sixphere.com>
2017-06-29 09:36:27 +0200

    Refactoring of code to use ES6

--- collaborat-io.js
+++ collaborat-io.js
@@ -1,47 +1,48 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Collaboratio = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 module.exports = Collaboratio;
 
-/**
- * Collaboratio constructor
- * @param {SocketIo} socket Socket.io socket
- */
-function Collaboratio(socket) {
-	this.socket = socket;
-	this.managers = [];
+class Collaboratio {
 
-	this.loadEventManagers();
-	this.exposeChildren();
-}
+	/**
+	 * Collaboratio constructor
+	 * @param {SocketIo} socket Socket.io socket
+	 */
+	constructor(socket) {
+		this.socket = socket;
+		this.managers = [];
 
-/**
- * Load the events managers from /lib/managers folder
- */
-Collaboratio.prototype.loadEventManagers = function () {
-	var requires = require('./lib/managers-loader');
-	for (var managerName in requires) {
-		if (requires.hasOwnProperty(managerName)) {
-			this.managers.push(requires[managerName](this.socket));
+		this.loadEventManagers();
+		this.exposeChildren();
+	}
+
+	/**
+	* Load the events managers from /lib/managers folder
+	*/
+	loadEventManagers () {
+		let requires = require('./lib/managers-loader');
+		for (let manager of requires) {
+				this.managers.push(manager(this.socket));
 		}
 	}
-};
 
-/**
- * Expose the manager functionalities to outer
- */
-Collaboratio.prototype.exposeChildren = function () {
-	this.managers.forEach(function (manager) {
-		extend(this, manager.expose());
-	}.bind(this));
-};
+	/**
+	* Expose the manager functionalities to outer
+	*/
+	exposeChildren () {
+		let extend = (obj, extension) => {
+			for (var i in extension) {
+			  if (extension.hasOwnProperty(i)) {
+			     obj[i] = extension[i];
+			  }
+			}
+		}
 
-function extend(obj, extension) {
-	for (var i in extension) {
-	  if (extension.hasOwnProperty(i)) {
-	     obj[i] = extension[i];
-	  }
-	}
+		for (let manager of managers) {
+			extend(this, manager.expose());
+		}
+	};
 }
 
 },{"./lib/managers-loader":3}],2:[function(require,module,exports){
@@ -49,22 +50,27 @@ function extend(obj, extension) {
 
 module.exports = CollaboratioEvent;
 
-function CollaboratioEvent(eventName) {
-	if(!(this instanceof CollaboratioEvent)) return new CollaboratioEvent(eventName);
+class CollaboratioEvent {
 
-	this.subscriptions = [];
-	this.loaded = false;
-	this.name = eventName;
+	constructor(eventName) {
+		this.subscriptions = [];
+		this.loaded = false;
+		this.name = eventName;
+	}
+
+	subscribe (fn) {
+		if(typeof fn == 'function')
+			this.subscriptions.push(fn);
+
+		return this;
+	}
+
+	call (data) {
+		for (let subscription of subscriptions) {
+			subscription(data);
+		}
+	}
 }
-
-CollaboratioEvent.prototype.subscribe = function (fn) {
-	if(typeof fn == 'function') this.subscriptions.push(fn);
-	return this;
-};
-
-CollaboratioEvent.prototype.call = function (data) {
-	this.subscriptions.forEach(function (subscription) { subscription(data); });
-};
 
 },{}],3:[function(require,module,exports){
 'use strict';
@@ -74,92 +80,88 @@ module.exports.rooms = require('./managers/rooms');
 },{"./managers/rooms":4}],4:[function(require,module,exports){
 'use strict';
 
-var CollaboratioEvent = require('../collaboratioEvent');
+let CollaboratioEvent = require('../collaboratioEvent');
 
 module.exports = RoomsManager;
 
-/**
- * RoomsManager constructor
- */
-function RoomsManager (socket){
-	if(!(this instanceof RoomsManager)) return new RoomsManager(socket);
+class RoomsManager {
 
-	this.socket = socket;
-	this.prefix = 'room';
-	this.events = {};
-
-}
-
-/**
- * Given a name, get the event with this name or create one
- * @param  {String} eventName 	Event's name
- * @return {CollaboratioEvent}	Event
- */
-RoomsManager.prototype.getOrCreateEvent = function (eventName) {
-	if (!this.events.hasOwnProperty(eventName)) {
-		this.events[eventName] = new CollaboratioEvent(eventName);
-		registerEvent(this, this.events[eventName]);
-		this.events[eventName].loaded = true;
+	/**
+	 * RoomsManager constructor
+	 * @param  socket SocketIO
+	 */
+	constructor(socket) {
+		this.socket = socket;
+		this.prefix = 'room';
+		this.events = {};
 	}
-	return this.events[eventName];
-};
 
-/**
- * Subscribe a function to an event
- * @param  {String}   eventName Event's name
- * @param  {Function} fn        Function
- */
-RoomsManager.prototype.subscribe = function (eventName, fn) {
-	this.getOrCreateEvent(eventName).subscribe(fn);
-};
+	/**
+	* Given a name, get the event with this name or create one
+	*
+	* USAGE:
+	* 	collaboratio.on('create')
+	* 		.subscribe((data) => bar(data))
+	* 		.subscribe((data) => foo(data))
+	*
+	* @param  {String} eventName 	Event's name
+	* @return {CollaboratioEvent}	Event
+	*/
+	on (eventName) {
 
+		/**
+		* Register the event in a Socket
+		* @param  {RoomsManager} manager Manager
+		* @param  {CollaboratioEvent} event   Event
+		*/
+		let registerEvent = (manager, event) => {
+			manager.socket.on(`${manager.prefix}:${event.name}`, (data) => {
+				this.getOrCreateEvent(event.name).call(data);
+			});
+		}
 
-/**
- * Register the event in a Socket
- * @param  {RoomsManager} manager Manager
- * @param  {CollaboratioEvent} event   Event
- */
-function registerEvent(manager, event) {
-	manager.socket.on(manager.prefix + ':' +  event.name, function (data) {
-		this.getOrCreateEvent(event.name).call(data);
-	}.bind(manager));
-}
+		if (!this.events.hasOwnProperty(eventName)) {
+			this.events[eventName] = new CollaboratioEvent(eventName);
+			registerEvent(this, this.events[eventName]);
+			this.events[eventName].loaded = true;
+		}
+		return this.events[eventName];
+	}
 
-/**
- * Expose functionality
- * @return {object} Object, which has an attribute labeled as rooms. Rooms contains as attributes each of RoomsManager's functionalities.
- */
-RoomsManager.prototype.expose = function () {
-
-	var createRoom = function (roomName) {
+	createRoom (roomName) {
 		if(!roomName) return false;
 		this.socket.emit('room:create', roomName);
 		return true;
-	}.bind(this);
+	}
 
-	var joinRoom = function (roomName) {
+	joinRoom (roomName) {
 		if(!roomName) return false;
 		this.socket.emit('room:join', roomName);
 		return true;
-	}.bind(this);
+	}
 
-	var listRooms = function () {
+	listRooms () {
 		this.socket.emit('room:list');
-	}.bind(this);
+	}
 
-	return {
-		rooms : {
-			create : createRoom,
-			join : joinRoom,
-			list : listRooms,
-			subscribe: this.subscribe.bind(this)
-			/*
-			remove: removeRoom,
-			get: getRoom,
-			exists: existsRoom*/
+	/**
+	* Expose functionality
+	* @return {object} Object, which has an attribute labeled as rooms. Rooms contains as attributes each of RoomsManager's functionalities.
+	*/
+	expose () {
+		return {
+			rooms : {
+				create : this.createRoom,
+				join : this.joinRoom,
+				list : this.listRooms,
+				on : this.on
+				/*
+				remove: removeRoom,
+				get: getRoom,
+				exists: existsRoom*/
+			}
 		}
 	}
 }
 
-},{"../collaboratioEvent":2}]},{},[1])(1)
-});
+},{"../collaboratioEvent":2}]},{},[1]);
--- index.js
+++ index.js
@@ -2,43 +2,44 @@
 
 module.exports = Collaboratio;
 
-/**
- * Collaboratio constructor
- * @param {SocketIo} socket Socket.io socket
- */
-function Collaboratio(socket) {
-	this.socket = socket;
-	this.managers = [];
+class Collaboratio {
 
-	this.loadEventManagers();
-	this.exposeChildren();
-}
+	/**
+	 * Collaboratio constructor
+	 * @param {SocketIo} socket Socket.io socket
+	 */
+	constructor(socket) {
+		this.socket = socket;
+		this.managers = [];
 
-/**
- * Load the events managers from /lib/managers folder
- */
-Collaboratio.prototype.loadEventManagers = function () {
-	var requires = require('./lib/managers-loader');
-	for (var managerName in requires) {
-		if (requires.hasOwnProperty(managerName)) {
-			this.managers.push(requires[managerName](this.socket));
+		this.loadEventManagers();
+		this.exposeChildren();
+	}
+
+	/**
+	* Load the events managers from /lib/managers folder
+	*/
+	loadEventManagers () {
+		let requires = require('./lib/managers-loader');
+		for (let manager of requires) {
+				this.managers.push(manager(this.socket));
 		}
 	}
-};
 
-/**
- * Expose the manager functionalities to outer
- */
-Collaboratio.prototype.exposeChildren = function () {
-	this.managers.forEach(function (manager) {
-		extend(this, manager.expose());
-	}.bind(this));
-};
+	/**
+	* Expose the manager functionalities to outer
+	*/
+	exposeChildren () {
+		let extend = (obj, extension) => {
+			for (var i in extension) {
+			  if (extension.hasOwnProperty(i)) {
+			     obj[i] = extension[i];
+			  }
+			}
+		}
 
-function extend(obj, extension) {
-	for (var i in extension) {
-	  if (extension.hasOwnProperty(i)) {
-	     obj[i] = extension[i];
-	  }
-	}
+		for (let manager of managers) {
+			extend(this, manager.expose());
+		}
+	};
 }
